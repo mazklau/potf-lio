54f8cc97ef8059714aec501f3e2f109e3e99ce59
Matheus Guilherme Sousa Bezerra <84602548+mazklau@users.noreply.github.com>
2023-06-13 23:18:27 -0300

    atualizei
    atualizei

--- js/i.js
+++ js/i.js
@@ -45,4 +45,4 @@ ScrollReveal({
 
 ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
 
-ScrollReveal().reveal('.home-img, .services-conteinar, .portfolio-box, .contact from', { origin: 'bottom' });
+ScrollReveal().reveal('.home-img, .about-img, .services-conteinar, .portfolio-box, .contact from', { origin: 'bottom' });
